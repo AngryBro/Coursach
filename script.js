46357b94152270413165ac6441ab3e6aa918cfd8
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-05-03 19:54:13 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+// function f1(x,y,a) {
+	// return a*x-y-x*(x*x+y*y);
+// }
+// function f2(x,y,a) {
+	// return a*y+x-y*(x*x+y*y);
+// }
 function f1(x,y,a) {
-	return a*x-y-x*(x*x+y*y);
+	return a*x-y+x*(x*x+y*y);
 }
 function f2(x,y,a) {
-	return a*y+x-y*(x*x+y*y);
+	return a*y+x+y*(x*x+y*y);
 }
 function euler(a,b,n,f1,f2,par) {
 	var points = [new Point(0.1,0.1)];
-	var dt = 1/1000;
+	var dt = 1/10000;
 	for(var k = 1; k<=n; k++) {
 		var xkm1 = points[k-1].x;
 		var ykm1 = points[k-1].y;
@@ -21,5 +27,5 @@ var svg = new SVG(1000, 1000);
 var oxy = new OXY(svg, 2, 20, 3, 'x', 'y', 2);
 svg.Create();
 oxy.Draw(svg);
-var points = euler(1,50,1000000,f1,f2,-0.5);
-oxy.Plot ( svg, points, 10, 1, 'blue' );
+var points = euler(1,50,1000000,f1,f2,0.005);
+oxy.Plot ( svg, points, 10, 3, 'blue' );
